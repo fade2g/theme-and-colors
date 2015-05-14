b6af161115c7b169e400fc1b36634d8d9fb8d043
fade2g <holger.heymanns@gmail.com>
2015-05-14 21:19:43 +0200

    cleaned up gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,120 +1,17 @@
 'use strict';
-// generated on 2014-06-18 using generator-gulp-webapp 0.1.0
-
 var gulp = require('gulp');
+var path = require('path');
 
 var styles_root = '.';
+var main_file = "custom.less";
 
 // load plugins
 var $ = require('gulp-load-plugins')();
 
 gulp.task('styles', function () {
-    return gulp.src(styles_root + '/custom.less')
+    return gulp.src(path.join(styles_root, main_file))
         .pipe($.less())
         .pipe($.autoprefixer('last 1 version'))
         .pipe($.size())
         .pipe(gulp.dest(styles_root));
-});
-
-gulp.task('html', ['styles','copy-fonts', 'scripts'], function () {
-    var jsFilter = $.filter('**/*.js');
-    var cssFilter = $.filter('**/*.css');
-
-    return gulp.src(static_files_root + '/*.html')
-        .pipe($.useref.assets({searchPath: '{.tmp,' + static_files_root + '}'}))
-        .pipe(jsFilter)
-        .pipe($.uglify())
-        .pipe(jsFilter.restore())
-        .pipe(cssFilter)
-        .pipe($.csso())
-        .pipe(cssFilter.restore())
-        .pipe($.useref.restore())
-        .pipe($.useref())
-        .pipe(gulp.dest('dist'))
-        .pipe($.size());
-});
-
-gulp.task('images', function () {
-    return gulp.src(static_files_root + '/images/**/*')
-        .pipe($.cache($.imagemin({
-            optimizationLevel: 3,
-            progressive: true,
-            interlaced: true
-        })))
-        .pipe(gulp.dest('dist/images'))
-        .pipe($.size());
-});
-
-gulp.task('fonts', function () {
-    return $.bowerFiles()
-        .pipe($.filter('**/*.{eot,svg,ttf,woff}'))
-        .pipe($.flatten())
-        .pipe(gulp.dest('dist/fonts'))
-        .pipe($.size());
-});
-
-gulp.task('extras', function () {
-    return gulp.src([static_files_root + '/*.*', '!' + static_files_root + '/*.html'], { dot: true })
-        .pipe(gulp.dest('dist'));
-});
-
-gulp.task('clean', function () {
-    return gulp.src(['.tmp', 'dist'], { read: false }).pipe($.clean());
-});
-
-gulp.task('build', ['html', 'images', 'fonts', 'extras']);
-
-gulp.task('default', ['clean'], function () {
-    gulp.start('build');
-});
-
-gulp.task('connect', function () {
-    var connect = require('connect');
-    var app = connect()
-        .use(require('connect-livereload')({ port: 35729 }))
-        .use(connect.static(static_files_root))
-        .use('/bower_components', connect.static(static_bower_root))    // serve bower components from different root
-        .use('/fonts', connect.static(static_bower_fontawsome_root))    // serve fonts needed because of font-awesome.css, gulp had done this before
-        .use(connect.static('.tmp'))
-        .use(connect.directory(static_files_root));
-
-    require('http').createServer(app)
-        .listen(9000)
-        .on('listening', function () {
-            console.log('Started connect web server on http://localhost:9000');
-        });
-});
-
-gulp.task('serve', ['connect'], function () {
-    require('opn')('http://localhost:9000');
-});
-
-// inject bower components
-gulp.task('wiredep', function () {
-    var wiredep = require('wiredep').stream;
-
-    gulp.src(static_files_root + '/*.html')
-        .pipe(wiredep({
-            directory: static_bower_root
-        }))
-        .pipe(gulp.dest(static_files_root));
-});
-
-gulp.task('watch', ['connect', 'serve'], function () {
-    var server = $.livereload();
-
-    // watch for changes
-    gulp.watch([
-        static_files_root + '/*.html',
-        '.tmp/styles/**/*.css',
-        static_files_root + '/scripts/**/*.js',
-        static_files_root + '/images/**/*'
-    ]).on('change', function (file) {
-        server.changed(file.path);
-    });
-
-    gulp.watch(static_files_root + '/styles/**/*.less', ['styles']);
-    gulp.watch(static_files_root + '/scripts/**/*.js', ['scripts']);
-    gulp.watch(static_files_root + '/images/**/*', ['images']);
-    gulp.watch('bower.json', ['wiredep']);
 });
