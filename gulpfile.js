40fdb1fae3b414beea460725917920e5c3f7f973
fade2g <holger.heymanns@gmail.com>
2015-05-19 21:49:43 +0200

    - gulpfile with watcher - added custom style für post h2 headings - FTP is probably wron, must be mysql

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,23 @@
 'use strict';
 var gulp = require('gulp');
 var path = require('path');
+var less = require('gulp-less');
+var watch = require('gulp-watch');
+var autoprefixer  = require('gulp-autoprefixer');
+var size = require('gulp-size');
+
 
 var styles_root = '.';
 var main_file = "custom.less";
 
-// load plugins
-var $ = require('gulp-load-plugins')();
-
 gulp.task('styles', function () {
     return gulp.src(path.join(styles_root, main_file))
-        .pipe($.less())
-        .pipe($.autoprefixer('last 1 version'))
-        .pipe($.size())
-        .pipe(gulp.dest(styles_root));
+        .pipe(less())
+        .pipe(autoprefixer('last 1 version'))
+        .pipe(size())
+        .pipe(gulp.dest(styles_root))
+});
+
+gulp.task('watch', function() {
+   gulp.watch('./*.less', ['styles'])
 });
